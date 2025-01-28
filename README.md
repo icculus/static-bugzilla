fa0edc20e4d672897f2091ae710ad8b6ab46fe3f
Ryan C. Gordon <icculus@icculus.org>
2025-01-28 12:03:06 -0500

    Initial add. PHP scripts not written yet.

---
+++ README.md
@@ -0,0 +1,114 @@
+# static-bugzilla
+
+## What is this?
+
+This is a hacky little piece of code I wrote to convert my ancient and
+abandoned Bugzilla installation into a static, read-only archive.
+
+This let me decommission the Bugzilla instance but leave the data publically
+available, keeping the most important URLs continuing to function. As an added
+benefit, the data serves faster and the server no longer needs to keep
+Bugzilla, MySQL, jobqueue.pl, etc, running.
+
+It might work for you.
+
+It pulls all the data it needs from the public website, so you can do it
+without direct access to the database. This means you could probably use it
+to archive someone else's Bugzilla install that you don't control, given time
+and bandwidth to pull everything.
+
+It is intended to make the individual bug pages and attachments available on
+their original URLs; everything else goes to a 404 page.
+
+## How to use this.
+
+- Clone this repository somewhere. Make sure perl and the curl command line
+  tool are installed. I ran this on Linux, it probably works on macOS, Windows
+  will probably need some small effort to set up.
+- If you intend to replace Bugzilla instead of just keep a static archive for
+  yourself, then on the Bugzilla webserver, you will need a way to run PHP
+  scripts. You will no longer need Perl, MySQL, or cgi-bin support.
+- Edit the top of `make_bugzilla_static_readonly.pl`. Look for the block
+  that says `# EDIT THESE LINES TO FIT YOUR BUGZILLA INSTALL!` ... read each
+  line and change it to match your target installation.
+- Run `make_bugzilla_static_readonly.pl` from the directory it is installed in.
+  It will create directories in the same place as it works. Make sure you can
+  access the Bugzilla installation over the network from this computer.
+- The script will begin pulling in data it needs from Bugzilla. If the script
+  is interrupted, it will pick up where it left off next time. It doesn't
+  move files into place until they are ready, so a power outage shouldn't
+  leave data half-prepared.
+- Downloading may take significant time. Running this script _directly on the
+  webserver itself_ (so no bandwidth delays) still took about four hours to
+  download 6678 bugs and 3639 attachments, as each HTTP request to Bugzilla
+  can take a few seconds. I shudder at running this against mozilla.org's
+  bugtracker which is dangerously close to 2 million bugs at the time of this
+  writing.
+- After download, the script will start generating static HTML pages for each
+  bug. This is pretty fast; my laptop chewed through 6678 bugs in 38 seconds,
+  but like the downloads, it is able to pick up where it left off if
+  interrupted.
+
+If you're replacing Bugzilla:
+
+- Edit 404.html and replace `noreply@example.com` with your email address (or
+  change the text in some other way).
+- After the script runs successfully, shut down Bugzilla on your server and
+  move the install out of the way. Copy this directory in its place, so that
+  show_bug.php will serve from the Bugzilla instance's root URL. The attachments
+  and bugs-html directories that the script generated should be here, too.
+- Change your webserver install so that show_bug.cgi requests go to
+  show_bug.php and attachment.cgi requests go to attachment.php...and that
+  all of these requests run as PHP pages. Note that many Bugzilla installations
+  have a second domain for attachments (for some security reason I never
+  understood), so attachment.cgi might run from, say, bugzilla.example.com and
+  also bugzilla-attachments.example.com, depending on your site's config.)
+- Make sure the webserver serves 404.html for all 404 responses, which
+  explains that Bugzilla is gone and most of its URLs are no longer available.
+  Feel free to edit this 404.html as appropriate.
+- Make sure the webserver serves images/favicon.ico and has the correct mimetype.
+- There is an Apache2 .htaccess file to block off files that shouldn't be
+  accessed directly on the web; you might have to deal with yourself this if
+  not using Apache.
+
+
+Once you're happy with everything:
+
+- You can delete `original-bugs-html`...but it is safe to leave it. Please
+  note that you won't be able to generate this data again unless you put the
+  original Bugzilla install back in place, so it might be better to leave it
+  here, in case you want to tweak the template or fix a script bug and rebuild
+  the static pages, etc.
+
+
+## I have bugs that are just spam that bots posted.
+
+Delete the static HTML files for them and the PHP code will report it as an
+invalid bug. Since re-running the script will regenerate the static HTML, you
+can also put a zero-byte file named BUGNUMBER-SPAM in the directory next to the
+static HTML (so bug number 6312 would have a file located at
+`bugs-html/6/3/6312-SPAM`). If this file exists, the PHP code will treat the
+bug report number as if it's invalid.
+
+
+## My bugzilla looked different.
+
+You probably have to hand-edit template.html to match your site. This might
+take some effort. Pay attention for `@symbol@` things in that file; those are
+replaced with bug-specific data when the static pages are generated.
+
+
+## What if this doesn't work?
+
+This worked for my Bugzilla install, but the code is fragile in many ways, so
+a different version of Bugzilla, or some theme, or even just a quirk of a bug
+report that I didn't run into, etc, might break it.
+
+If you have problems, [file a bug](https://github.com/icculus/static-bugzilla/issues),
+or better yet, [send a patch](https://github.com/icculus/static-bugzilla/pulls), and
+we can talk it through.
+
+## Thanks!
+
+--ryan.
+
---
+++ make_bugzilla_static_readonly.pl
@@ -0,0 +1,334 @@
+#!/usr/bin/perl -w
+
+# static-bugzilla; a hacky way to make a Bugzilla install a read-only archive.
+#
+# Please read README.md for how to use this.
+#
+# Please see the file LICENSE.txt in the source's root directory.
+#
+#  This file written by Ryan C. Gordon.
+
+use warnings;
+use strict;
+
+# EDIT THESE LINES TO FIT YOUR BUGZILLA INSTALL!
+my $sitename = 'bugzilla.icculus.org';  # This can also be a real name like "The Icculus Bugtracker" or whatever.
+my $total_attachments = 3639;  # the highest attachment id provided by the bugtracker. Set this high until the script fails if you don't know.
+my $total_bugs = 6678;  # the highest bug id provided by the bugtracker. Set this high until the script fails if you don't know.
+my $bugzilla_url = 'https://bugzilla.icculus.org';  # the base URL for the bugtracker.
+my $bugzilla_attachments_url = 'https://bugzilla-attachments.icculus.org';  # the base URL for the bugtracker's attachments, which might be different. Click an attachment and see where it takes you if you don't know.
+# EDIT THESE LINES TO FIT YOUR BUGZILLA INSTALL!
+
+
+
+if ( ! -f "images/favicon.ico" ) {
+    system("mkdir -p 'images'") == 0 or die("Failed to mkdir -p 'images'");
+    print("Collecting favicon.ico...\n");
+    system("curl -o 'data-in-progress' '$bugzilla_url/images/favicon.ico'") == 0 or die("curl failed on favicon.ico!");
+    system("mv 'data-in-progress' 'images/favicon.ico'");
+}
+print("favicon.ico is collected!\n\n");
+
+print("Collecting attachments...\n");
+my $attachmentsdir = 'attachments';
+for (my $i = 1; $i <= $total_attachments; $i++) {
+    my $dir = "$attachmentsdir/" . int($i / 1000) . '/' . int(($i % 1000) / 100) . '/' . $i;
+    next if ( -f "$dir/data" );  # already done with this one.
+    print(" - Collecting attachment $i ...\n");
+    my @curlhead = split /\r\n/, `curl -I -s -S "$bugzilla_attachments_url/attachment.cgi?id=$i"`;
+    die("HTTP HEAD failed on attachment $i") if not @curlhead;    
+    die("Unexpected HTTP response '{$curlhead[0]}' on attachment $i") if $curlhead[0] ne 'HTTP/1.1 200 OK';
+
+    my %response = ();
+    foreach (@curlhead) {
+        if (/\A(.*?)\:\s+(.*)\Z/) {
+            $response{$1} = $2;
+        }
+    }
+
+    system("mkdir -p '$dir'") == 0 or die("Failed to mkdir -p '$dir'");
+    open(FH, '>', "$dir/content-disposition") or die("Failed to open '$dir/content-disposition': $!"); print FH $response{'Content-disposition'}; close(FH);
+    open(FH, '>', "$dir/content-type") or die("Failed to open '$dir/content-type': $!"); print FH $response{'Content-Type'}; close(FH);
+    system("curl -o 'data-in-progress' '$bugzilla_attachments_url/attachment.cgi?id=$i'") == 0 or die("curl failed on attachment $i!");
+    system("mv 'data-in-progress' '$dir/data'");
+}
+print("Attachments are all collected!\n\n");
+
+print("Collecting bug reports...\n");
+my $origdir = 'original-bugs-html';
+mkdir($origdir) if ( ! -d $origdir );
+for (my $i = 1; $i <= $total_bugs; $i++) {
+    next if ( -f "$origdir/$i" );  # already done with this one.
+    print(" - Collecting bug $i ...\n");
+    my $url = "$bugzilla_url/show_bug.cgi?id=$i";
+    system("curl -o 'data-in-progress' '$url'") == 0 or die("curl failed on bug $i!");
+    system("mv 'data-in-progress' '$origdir/$i'");
+}
+print("Bug reports are all collected!\n\n");
+
+
+print("Generating static HTML files...\n\n");
+my $finaldir = "bugs-html";
+mkdir($finaldir) if ( ! -d $finaldir );
+for (my $i = 1; $i <= $total_bugs; $i++) {
+    my $dir = "$finaldir/" . int($i / 1000) . '/' . int(($i % 1000) / 100);
+    #$dir = "$finaldir";
+    next if ( -f "$dir/$i" );  # already done with this one.
+    print(" - Generating static HTML for bug $i ...\n");
+    system("mkdir -p '$dir'") == 0 or die("Failed to mkdir -p '$dir'");
+    open(FHORIGIN, '<', "$origdir/$i") or die("Failed to open '$dir/$i': $!");
+    my $bugtitle = undef;
+    my $bugcomments = undef;
+    my $bugstatus = undef;
+    my $product = undef;
+    my $classification = undef;
+    my $component = undef;
+    my $lastmod = undef;
+    my $reportedtime = undef;
+    my $reportedby = undef;
+    my $cclist = undef;
+    my $version = undef;
+    my $hardware = undef;
+    my $importance = undef;
+    my $assignee = undef;
+    my $qacontact = undef;
+    my $alias = undef;
+    my $url = undef;
+    my $blocks = undef;
+    my $dependson = undef;
+    my $seealso = undef;
+    my $attachments = '';
+    my $duplicates = '';
+
+    my $skip_this_bug = 0;
+
+    while (<FHORIGIN>) {
+        chomp;
+        #print("$_\n");
+
+        $skip_this_bug = 1, last if (/\A\s*<title\>Missing Bug ID\<\/title\>\s*\Z/);
+
+        if (/\A\<table class\=\"bz_comment_table\"\>\Z/) {
+            $bugcomments = "$_\n";
+            while (<FHORIGIN>) {
+                $bugcomments .= $_;
+                chomp;
+                last if /\A\<\/tr\>\<\/table\>\Z/;
+            }
+        } elsif (/\A\s*\<span id\=\"information\" class\=\"header_addl_info\"\>Last modified: (.*?)\<\/span\>\Z/) {
+            $lastmod = $1;
+        } elsif (/\A\s*\<span id\=\"subtitle\" class=\"subheader\"\>(.*?)\<\/span\>\Z/) {
+            $bugtitle = $1;
+        } elsif (/\A\s*<title\>\d+ \&ndash\; (.*?)\<\/title\>\s*\Z/) {
+            $bugtitle = $1;
+        } elsif (/\A\s*\<span id\=\"static_bug_status\"\>(.*)\Z/) {
+            $bugstatus = $1;
+            while (<FHORIGIN>) {
+                chomp;
+                s/\A\s*//;
+                s/\s*\Z//;
+                last if $_ eq '</span>';
+                $bugstatus .= " $_";
+            }
+        } elsif (/\A\s*\<span id\=\"blocked_input_area\"\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Blocks parsing") if not $tmp =~ /\A\s*\<\/span\>\s*\Z/;
+            $blocks = '';
+            while (<FHORIGIN>) {
+                chomp;
+                last if /\A\s*\<\/td\>\s*\Z/;
+                $blocks .= "$_\n";
+            }
+        } elsif (/\A\s*\<span id\=\"dependson_input_area\"\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Dependson parsing") if not $tmp =~ /\A\s*\<\/span\>\s*\Z/;
+            $dependson = '';
+            while (<FHORIGIN>) {
+                chomp;
+                last if /\A\s*\<\/td\>\s*\Z/;
+                $dependson .= "$_\n";
+            }
+        } elsif (/\A\s*\<span id\=\"duplicates\"\>/) {
+            my $dupcount = 0;
+            my $dups = "$_\n";
+            while (<FHORIGIN>) {
+                $dups .= $_;
+                $dupcount++ if /href\=\"show_bug.cgi\?id=\d+/;
+                chomp;
+                s/\A\s*//;
+                s/\s*\Z//;
+                last if $_ eq '</span>';
+            }
+            $duplicates = "<tr><th class=\"field_label\"><label>Duplicates ($dupcount)</label>:\n</th><td class=\"field_value\">\n$dups</td></tr>";
+        } elsif (/\A\s*CC List\:/) {
+            my $cc = '';
+            while (<FHORIGIN>) {
+                if (/\<script/) {
+                    while (<FHORIGIN>) {
+                        last if /\<\/script\>/;
+                    }
+                    next;
+                }
+                s/class\=\"bz_default_hidden\"//g;
+                $cc .= $_;
+                chomp;
+                s/\A\s*//;
+                s/\s*\Z//;
+                last if $_ eq '</tr>';
+            }
+            $cclist = "<th class=\"field_label\"><label  accesskey=\"a\">CC List:$cc\n";
+        } elsif (/\A\s*\<td\>(.*?) by \<span class\=\"vcard\"\>(.*?)\Z/) {
+            $reportedtime = $1;
+            $reportedby = $2;
+        } elsif (/\A\s*id\=\"field_container_product\" >(.*?)\s*\Z/) {
+            $product = $1;
+        } elsif (/\A\s*id\=\"field_container_classification\" >(.*?)\s*\Z/) {
+            $classification = $1;
+        } elsif (/\A\s*id\=\"field_container_component\" >(.*?)\s*\Z/) {
+            $component = $1;
+        } elsif (/\A\s*\>Version\:\<\/a\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Version parsing") if $tmp ne "\n";
+            $tmp = <FHORIGIN>; die("unexpected Version parsing") if $tmp ne "</th>\n";
+            $tmp = <FHORIGIN>; die("unexpected Version parsing") if not $tmp =~ s/\A\<td\>//;
+            $version = $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Version parsing") if $tmp ne "  </td>\n";
+        } elsif (/\A\s*\>Hardware\:\<\/a\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Hardware parsing") if $tmp ne "\n";
+            $tmp = <FHORIGIN>; die("unexpected Hardware parsing") if $tmp ne "</th>\n";
+            $tmp = <FHORIGIN>; die("unexpected Hardware parsing") if not $tmp =~ s/\A\s*\<td class\=\"field_value\"\>//;
+            $hardware = $tmp;
+            while (<FHORIGIN>) {
+                chomp;
+                s/\A\s*//;
+                s/\s*\Z//;
+                last if $_ eq '</td>';
+                $hardware .= " $_";
+            }
+        } elsif (s/\A\s*\<span id\=\"bz_url_input_area\"\>//) {
+            $url = $_;
+            while (<FHORIGIN>) {
+                chomp;
+                last if /\A\s*\<\/span\>\Z/;
+                $url .= "$_\n";
+            }
+        } elsif (/\A\s*\>Alias\:\<\/a\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Alias parsing") if $tmp ne "\n";
+            $tmp = <FHORIGIN>; die("unexpected Alias parsing") if $tmp ne "</th>\n";
+            $tmp = <FHORIGIN>; die("unexpected Alias parsing") if not $tmp =~ /\A\s*\<td\>/;
+            $tmp = <FHORIGIN>; die("unexpected Alias parsing") if not $tmp =~ s/\A\s*(.*?)\s*\Z/$1/;
+            $alias = $tmp;
+        } elsif (/\A\s*\>Assignee\:\<\/a\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Assignee parsing") if $tmp ne "\n";
+            $tmp = <FHORIGIN>; die("unexpected Assignee parsing") if $tmp ne "</th>\n";
+            $tmp = <FHORIGIN>; die("unexpected Assignee parsing") if not $tmp =~ s/\A\s*\<td\>\<span class\=\"vcard\"\>(.*)\Z/$1/;
+            $assignee = $tmp;
+        } elsif (/\A\s*\>QA Contact\:\<\/a\>\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected QA Contact parsing") if $tmp ne "\n";
+            $tmp = <FHORIGIN>; die("unexpected QA Contact parsing") if $tmp ne "</th>\n";
+            $tmp = <FHORIGIN>; die("unexpected QA Contact parsing") if not $tmp =~ s/\A\s*\<td\>\<span class\=\"vcard\"\>(.*)\Z/$1/;
+            $qacontact = $tmp;
+        } elsif (/\A\s*\<a href\=\"page\.cgi\?id\=fields.html\#importance\"\>\<u\>I\<\/u\>mportance\<\/a\>\<\/label\>\:\s*\Z/) {
+            my $tmp;
+            $tmp = <FHORIGIN>; die("unexpected Importance parsing") if $tmp ne "      </th>\n";
+            $tmp = <FHORIGIN>; die("unexpected Importance parsing") if not $tmp =~ s/\A\s*\<td\>//;
+            $importance = $tmp;
+            while (<FHORIGIN>) {
+                chomp;
+                s/\A\s*//;
+                s/\s*\Z//;
+                last if $_ eq '</td>';
+                $importance .= " $_";
+            }
+        } elsif (s/\A\s*id\=\"field_container_see_also\"\s*\>//) {
+            $seealso = $_;
+            while (<FHORIGIN>) {
+                chomp;
+                last if /\A\s*\<\/td\>\Z/;
+                $seealso .= "$_\n";
+            }
+        } elsif (/\A\s*\<tr id\=\"(a\d+)\"/) {
+            if ($1 ne 'a0') { # a0 is the table header, covered elsewhere.
+                $attachments .= "$_\n";
+                while (<FHORIGIN>) {
+                    $attachments .= $_;
+                    chomp;
+                    $attachments .= "</tr>\n\n", last if /\A\s*\<\/td\>/;  # this chops out the "details" column.
+                }
+            }
+        }
+    }
+    close(FHORIGIN);
+
+    next if $skip_this_bug;
+
+    die("No bugtitle found in bug $i") if not defined $bugtitle;
+    die("No bugcomments found in bug $i") if not defined $bugcomments;
+    die("No bugstatus found in bug $i") if not defined $bugstatus;
+    die("No lastmod found in bug $i") if not defined $lastmod;
+    die("No reportedtime found in bug $i") if not defined $reportedtime;
+    die("No reportedby found in bug $i") if not defined $reportedby;
+    die("No cclist found in bug $i") if not defined $cclist;
+    die("No product found in bug $i") if not defined $product;
+    die("No component found in bug $i") if not defined $component;
+    die("No classification found in bug $i") if not defined $classification;
+    die("No version found in bug $i") if not defined $version;
+    die("No hardware found in bug $i") if not defined $hardware;
+    die("No importance found in bug $i") if not defined $importance;
+    die("No assignee found in bug $i") if not defined $assignee;
+    die("No qacontact found in bug $i") if not defined $qacontact;
+    die("No alias found in bug $i") if not defined $alias;
+    die("No url found in bug $i") if not defined $url;
+    die("No blocks found in bug $i") if not defined $blocks;
+    die("No dependson found in bug $i") if not defined $dependson;
+    die("No seealso found in bug $i") if not defined $seealso;
+
+    if ($attachments ne '') {
+        $attachments = "<table id=\"attachment_table\"><tr id=\"a0\"><th class=\"left\">Attachments</th></tr>\n$attachments\n</table>\n";
+    }
+
+    open(FHOUT, '>', 'data-in-progress') or die("Failed to open 'data-in-progress': $!");
+    open(FHTEMPLATEIN, '<', 'template.html') or die("Failed to open 'template.html': $!");
+    while (<FHTEMPLATEIN>) {
+        chomp;
+        s/\@bugnum\@/$i/g;
+        s/\@bugtitle\@/$bugtitle/g;
+        s/\@lastmod\@/$lastmod/g;
+        s/\@reportedtime\@/$reportedtime/g;
+        s/\@reportedby\@/$reportedby/g;
+        s/\@bugcomments\@/$bugcomments/g;
+        s/\@bugstatus\@/$bugstatus/g;
+        s/\@duplicates\@/$duplicates/g;
+        s/\@cclist\@/$cclist/g;
+        s/\@product\@/$product/g;
+        s/\@classification\@/$classification/g;
+        s/\@component\@/$component/g;
+        s/\@version\@/$version/g;
+        s/\@hardware\@/$hardware/g;
+        s/\@importance\@/$importance/g;
+        s/\@assignee\@/$assignee/g;
+        s/\@qacontact\@/$qacontact/g;
+        s/\@attachments\@/$attachments/g;
+        s/\@alias\@/$alias/g;
+        s/\@url\@/$url/g;
+        s/\@blocks\@/$blocks/g;
+        s/\@dependson\@/$dependson/g;
+        s/\@seealso\@/$seealso/g;
+        s/\@sitename\@/$sitename/g;
+
+        print FHOUT "$_\n";
+    }
+    close(FHTEMPLATEIN);
+    close(FHOUT);
+
+    system("mv 'data-in-progress' '$dir/$i'");
+}
+
+print("Static HTML pages are all generated!\n\n");
+
+print("\n\n\nAll done!\n\n\n");
+
